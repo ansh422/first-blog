776bc88318383af403582f4d7c64f63515970e2b
Ansh Gupta <52572592+ansh422@users.noreply.github.com>
2021-03-05 16:11:34 +0530

    Allowed Host changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'okmb+f_r2lzssxt&$+-)9ltk*v$*p9!wxoam=!ft#si6cqc&*m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
